5fd9e2fd2af844d16e23ca88275243b5ca6b5f28
Wojciech Wiński <wojciech.winski@gmail.com>
2017-01-30 00:08:35 +0100

    edit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
-
+  var editStatus = false;
+  var spans = $('section.scoreTable.navTable').find('span');
   function gameClass() {
     this.nr = 0;
     this.player = '';
@@ -56,7 +57,7 @@ $(document).ready(function(){
 
       }
     };
-    var spans = $('section.scoreTable.navTable').find('span');
+
     this.print = function (){
 
 
@@ -78,21 +79,31 @@ $(document).ready(function(){
       if (parseInt(this.levelContract) == 0) {
         spans.eq(3).html('pass');
         spans.eq(3).data('color','pass');
-        // console.log('pass',spans.eq(3).data('color'));
+
       } else if (this.colorContract == 'NT')  {
                   spans.eq(3).html(this.levelContract+'NT');
                   spans.eq(3).data('color','NT');
-                  // console.log('nt',spans.eq(3).data('color'));
+
              } else {
                spans.eq(3).html(this.levelContract+'&'+this.colorContract+';');
                spans.eq(3).data('color',this.colorContract);
-              //  console.log('kolor',spans.eq(3).data('color'));
+
              };
-      console.log('spans',spans);
-      if (this.doubled == true) spans.eq(3).addClass('doubled')
-      else spans.eq(3).removeClass('doubled');
-      if (this.redoubled == true) spans.eq(3).removeClass('doubled').addClass('redoubled')
-      else spans.eq(3).removeClass('redoubled');
+
+      // spans.eq(3).addClass('undoubled').removeClass('doubled').removeClass('redoubled');
+      if (this.double == true) {
+        spans.eq(3).addClass('doubled')
+      } else {
+        spans.eq(3).removeClass('doubled');
+      };
+      if (this.redouble == true) {
+        spans.eq(3).removeClass('doubled').addClass('redoubled')
+      } else {
+        spans.eq(3).removeClass('redoubled');
+      };
+
+      // console.log(spans.eq(3),this.doubled,this.redoubled);
+
       spans.eq(4).text(this.tricks);
 
       if (this.levelContract == 0) {
@@ -115,7 +126,8 @@ $(document).ready(function(){
       // if (ns)
       // spans.eq(2).text(this.nr);
       // spans.eq(3).text(this.nr);
-      console.log('print', this, spans.eq(3).data('color'));
+
+
       // spans[0].text(this.nr);
       // spans[1].text(this.player);
       // spans[2].text();
@@ -177,8 +189,8 @@ $(document).ready(function(){
     game0.levelContract = $(this).data('contract')[0];
     game0.colorContract = $(this).data('contract').slice(2);
 
-      game0.doubled = false;
-      game0.redoubled = false;
+      game0.double = false;
+      game0.redouble = false;
       $('[data-doubled]').css('color','black');
       $('[data-redoubled]').css('color','black');
 
@@ -190,12 +202,12 @@ $(document).ready(function(){
         if ($(this).data('doubled') == 'no') {
           $(this).data('doubled','yes');
           $(this).css('color','red');
-          game0.doubled = true;
+          game0.double = true;
         } else {
           $(this).data('doubled','no');
-          game0.doubled = false;
+          game0.double = false;
           $(this).css('color','black');
-          game0.redoubled = false;
+          game0.redouble = false;
           $('[data-redoubled]').css('color','black');
           $('[data-redoubled]').data('redoubled','no');
         }
@@ -210,11 +222,11 @@ $(document).ready(function(){
               if ($(this).data('redoubled') == 'no') {
                 $(this).data('redoubled','yes');
                 $(this).css('color','red');
-                game0.redoubled = true;
+                game0.redouble = true;
               } else {
                 $(this).data('redoubled','no');
                 $(this).css('color','black');
-                game0.redoubled = false;
+                game0.redouble = false;
               }
             }
             game0.print();
@@ -242,76 +254,107 @@ $(document).ready(function(){
   });
 
   $('.save h1').on('click',function(){
-    var $copy = $('section.scoreTable.navTable').clone();
+
+    var $copy = $('section.scoreTable.navTable').eq(0).clone(true);
     $copy.removeClass('navTable').addClass('saved');
     if (game0.nr < 10) $copy.find('span').eq(0).text("0"+game0.nr);
     $copy.css('border-bottom','1px solid lightgrey');
     $copy.append($("<span class='edit'>EDIT</span><span class='delete'>DELETE</span>"));
 
-    $copy.find('.delete').on('click',deleteSavedBar);
+                        $copy.find('.delete').on('click',deleteSavedBar);
 
-    $copy.find('.edit').on('click',function(){
-      var _nr             = parseInt($(this).parent().find('.number').eq(0).text());
-      var _player         = $(this).parent().find('.player').eq(0).text();
-      console.log('player',$(this).parent().find('.player').eq(0).text());
-      var _nsVulnarable;
-      var _weVulnarable;
-      var _levelContract;
-      var _colorContract;
-      var _double;
-      var _redouble;
-      var _handPoints;
-      var _tricks;
+                        $copy.find('.edit').on('click',function(){
+                          resetView();
+                          var _nr             = parseInt($(this).parent().find('.number').eq(0).text());
+                          var _player         = $(this).parent().find('.player').eq(0).text();
 
-      var $player  = $(this).parent().find('.player').eq(0);
-      var $oponent = $(this).parent().find('.oponent').eq(0);
-      if ($player.text() == 'NS') {
-        _nsVulnarable = $player.hasClass('vulnarable');
+                          var _nsVulnarable;
+                          var _weVulnarable;
+                          var _levelContract;
+                          var _colorContract;
+                          var _double;
+                          var _redouble;
+                          var _handPoints;
+                          var _tricks;
+
+                          var $player  = $(this).parent().find('.player').eq(0);
+                          var $oponent = $(this).parent().find('.oponent').eq(0);
+                          if ($player.text() == 'NS') {
+                            _nsVulnarable = $player.hasClass('vulnarable');
+                          } else {
+                            _weVulnarable = $player.hasClass('vulnarable');
+                          };
+                          if ($oponent.text() == 'NS') {
+                            _nsVulnarable = $oponent.hasClass('vulnarable');
+                          } else {
+                            _weVulnarable = $oponent.hasClass('vulnarable');
+                          };
+
+                          var $contract = $(this).parent().find('.contract').eq(0);
+
+                          if ($contract.text()=='pass') {
+                            _levelContract = 0;
+                            _double = false;
+                            _redouble = false;
+                          } else {
+                            _levelContract = parseInt($contract.text()[0]);
+                            _colorContract = $contract.data('color');
+
+                            if ($contract.hasClass('redoubled')) {
+                              _double = true;
+                              _redouble = true;
+                            } else if ($contract.hasClass('doubled')) {
+                              _double = true;
+                              _redouble = false;
+                            } else {
+                              _double = false;
+                              _redouble = false;
+                            }
+                          };
+
+                          _handPoints = parseInt($(this).parent().find('.handPoints').eq(0).text());
+                          _tricks = parseInt($(this).parent().find('.tricks').eq(0).text());
+
+
+                          // _nr, _player, _nsVulnarable, _weVulnarable, _levelContract, _colorContract, _double, _redouble, _handPoints, _tricks
+                          game0.init(_nr, _player, _nsVulnarable, _weVulnarable, _levelContract, _colorContract, _double, _redouble, _handPoints, _tricks);
+                          // game0.init(_nr,'NS',false,false,0,'pass', false, false, 20, 6);
+                          resetView();
+                          game0.print();
+                          editStatus = true;
+                          $('.save h1').text('SAVE (nr: '+game0.nr+')');
+                          $('.edit').hide();
+                          $('.delete').hide();
+
+                        });
+
+    if (editStatus == true) {
+      var nrToRemove = parseInt($copy.find('span.number').eq(0).text());
+
+      var nrLast = parseInt($('.saved').find('span.number').last().text());
+
+      if (nrToRemove == nrLast){
+        $('.saved').last().remove();
+        $copy.appendTo($('body'));
       } else {
-        _weVulnarable = $player.hasClass('vulnarable');
-      };
-      if ($oponent.text() == 'NS') {
-        _nsVulnarable = $oponent.hasClass('vulnarable');
-      } else {
-        _weVulnarable = $oponent.hasClass('vulnarable');
-      };
+          $('.saved').eq(nrToRemove-1).remove();
+          $copy.insertBefore($('.saved').eq(nrToRemove-1));
+      }
+      editStatus = false;
+      $('.save h1').text('SAVE');
+      $('.edit').show();
+      $('.delete').show();
+      game0.init(nrLast+1,'NS',false,false,0,'pass', false, false, 20, 6);
+      resetView();
 
-      var $contract = $(this).parent().find('.contract').eq(0);
-      if ($contract.text()=='pass') {
-        _levelContract = 0;
-        _double = false;
-        _redouble = false;
-      } else {
-        _levelContract = parseInt($contract.text()[0]);
-        _colorContract = $contract.data('color');
-        console.log('edit', $contract.html());
-        if ($contract.hasClass('redoubled')) {
-          _double = true;
-          _redouble = true;
-        } else if ($contract.hasClass('redoubled')) {
-          _double = true;
-          _redouble = false;
-        } else {
-          _double = false;
-          _redouble = false;
-        }
-      };
+    } else {
+        $copy.appendTo($('body'));
+        game0.init(game0.nr+1,'NS',false,false,0,'pass', false, false, 20, 6);
+        console.log('game',game0);
+        game0.print();
+        resetView();
 
-      _handPoints = $(this).parent().find('.handPoints').eq(0).text();
-      _tricks = parseInt($(this).parent().find('.tricks').eq(0).text());
-
-
-      // _nr, _player, _nsVulnarable, _weVulnarable, _levelContract, _colorContract, _double, _redouble, _handPoints, _tricks
-      game0.init(_nr, _player, _nsVulnarable, _weVulnarable, _levelContract, _colorContract, _double, _redouble, _handPoints, _tricks);
-      game0.print();
-      editStatus = true;
-      $('.save h1').text('SAVE (nr: '+game0.nr+')');
-      console.log(game0);
-    });
-
-    $copy.appendTo($('body'));
-
-    game0.init(++game0.nr,'NS',false,false,0,'pass', false, false, 20, 6);
+    }
     game0.print();
   });
 
@@ -325,7 +368,7 @@ $(document).ready(function(){
       recountTotalSums();
       game0.nr = $spanNumbers.length+1;
       game0.print();
-      console.log($spanNumbers);
+
 
   }
 
@@ -336,15 +379,12 @@ $(document).ready(function(){
     var $savedImpPoints = $('.saved').find('span.impPoints');
     var sumNS = 0;
     var sumWE = 0;
-    console.log('savedNS',$savedNS);
-    console.log('savedWE',$savedWE);
-    console.log('savedPlayer',$savedPlayer);
-    console.log('savedImpPoints',$savedImpPoints);
+
 
     for (var i=0;i < $savedNS.length;i++){
       if (($savedPlayer.eq(i).text() == 'NS') && ($savedImpPoints.eq(i).text() >= 0)) {
         sumNS = parseInt(sumNS) + parseInt($savedImpPoints.eq(i).text());
-        console.log('sumNS',sumNS);
+
       };
       if (($savedPlayer.eq(i).text() == 'NS') && (parseInt($savedImpPoints.eq(i).text()) < 0)) {
         sumWE -= parseInt($savedImpPoints.eq(i).text());
@@ -362,6 +402,60 @@ $(document).ready(function(){
 
   };
 
+  function resetView(){
+    // $('.playerName').css('background','green').text('NS');
+    if (($('.playerName').text == 'NS') && (game0.nsVulnarable)) $('.playerName').css('background','red');
+    if (($('.playerName').text == 'NS') && !(game0.nsVulnarable)) $('.playerName').css('background','green');
+    if (($('.playerName').text == 'WE') && (game0.weVulnarable)) $('.playerName').css('background','red');
+    if (($('.playerName').text == 'WE') && !(game0.weVulnarable)) $('.playerName').css('background','green');
+    if (game0.nsVulnarable) $('.NSvulnarable').css('background','green')
+    else $('.NSvulnarable').css('background','red');
+    if (game0.weVulnarable) $('.WEvulnarable').css('background','green')
+    else $('.WEvulnarable').css('background','red');
+    var index;
+    if (game0.colorContract=='clubs') {
+      index = game0.levelContract-1;
+    } else if (game0.colorContract=='diams') {
+              index = game0.levelContract-1+7;
+           } else if (game0.colorContract=='hearts') {
+                     index = game0.levelContract-1+14;
+                  } else if (game0.colorContract=='spades') {
+                            index = game0.levelContract-1+21;
+                         } else if (game0.colorContract=='NT') {
+                                   index = game0.levelContract-1+28;
+                                } else {
+                                  index = 35;
+                                }
+    $('[data-contract]').css('background','lightgrey').css('color','black');
+    $('[data-contract]').eq(index).css('background','grey').css('color','red');
+    if (game0.double == true) {
+      $('[data-doubled]').data('doubled','yes').css('color','red')
+    } else {
+      $('[data-doubled]').data('doubled','no').css('color','black');
+      $('[data-redoubled]').data('redoubled','no').css('color','black');
+    };
+    if (game0.redouble == true) {
+      $('[data-doubled]').data('doubled','yes').css('color','red');
+      $('[data-redoubled]').data('redoubled','yes').css('color','red');
+    } else {
+      $('[data-redoubled]').data('redoubled','no').css('color','black');
+    };
+    $('[data-tricks]').css('background','lightgrey').css('color','black');
+    $('[data-tricks]').eq(game0.tricks).css('background','grey').css('color','red');
+    $('[data-handpoints]').css('background','lightgrey').css('color','black');
+    $('[data-handpoints]').eq(game0.handPoints).css('background','grey').css('color','red');
 
+    // $('.NSvulnarable').css('background','green');
+    // $('.WEvulnarable').css('background','green');
+    // $('[data-contract]').css('background','lightgrey').css('color','black');
+    // $('[data-contract]').last().css('background','grey').css('color','red');
+    // $('[data-doubled]').data('doubled','no').css('color','black');
+    // $('[data-redoubled]').data('redoubled','no').css('color','black');
+    // $('[data-tricks]').css('background','lightgrey').css('color','black');
+    // $('[data-handpoints]').css('background','lightgrey').css('color','black');
+
+
+
+  };
 
 });
